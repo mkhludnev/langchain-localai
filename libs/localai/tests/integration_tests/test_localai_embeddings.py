373794da3384030f8262336cf1773336085d337b
Mikhail Khludnev <mkhludnev@users.noreply.github.com>
2024-12-17 15:46:37 +0300

    bulk embed_docs fix #2 (#3)
    * bulk embed_docs #2
    
    * let's run integration every time

--- libs/localai/tests/integration_tests/test_localai_embeddings.py
+++ libs/localai/tests/integration_tests/test_localai_embeddings.py
@@ -51,3 +51,50 @@ async def test_localai_proxy_aembed() -> None:
     eqq = await llm.aembed_query("foo bar")
     assert len(eqq) > 100
     assert eqq[0] != eqq[1]
+
+
+@pytest.mark.requires("openai")
+@pytest.mark.vcr
+def test_localai_embed_docs() -> None:
+    llm = LocalAIEmbeddings(
+        openai_api_key="foo", model="bge-m3", openai_api_base="https://foo.bar/v1"
+    )
+    docs1 = llm.embed_documents(["foo bar", "moo foo"])
+    for doc in docs1:
+        assert len(doc) > 100
+        assert doc[0] != doc[1]
+    assert len(docs1) == 2
+    assert docs1[0] != docs1[1]
+    docs2 = llm.embed_documents(["moo foo", "foo bar"])
+    for doc in docs2:
+        assert len(doc) > 100
+        assert doc[0] != doc[1]
+    assert len(docs2) == 2
+
+    assert docs2[0] != docs2[1]
+
+    assert docs1[0] == docs2[1]
+    assert docs1[1] == docs2[0]
+
+
+@pytest.mark.requires("openai")
+@pytest.mark.vcr
+async def test_localai_aembed_docs() -> None:
+    llm = LocalAIEmbeddings(
+        openai_api_key="foo", model="bge-m3", openai_api_base="https://foo.bar/v1"
+    )
+    docs1 = await llm.aembed_documents(["foo bar", "moo foo"])
+    for doc in docs1:
+        assert len(doc) > 100
+        assert doc[0] != doc[1]
+    assert len(docs1) == 2
+    assert docs1[0] != docs1[1]
+    docs2 = await llm.aembed_documents(["moo foo", "foo bar"])
+    for doc in docs2:
+        assert len(doc) > 100
+        assert doc[0] != doc[1]
+    assert len(docs2) == 2
+    assert docs2[0] != docs2[1]
+
+    assert docs1[0] == docs2[1]
+    assert docs1[1] == docs2[0]
